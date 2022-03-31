2c6c828d481f1ac13f33e5b0db61028b128b7578
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-03-31 23:20:26 +0900

    Feat: Logo 컴포넌트 생성

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LogoImg from '../assets/images/logo.svg';
+
+const Logo = () => {
+	return <img src={LogoImg} alt="Jobify" className="logo" />;
+};
+
+export default Logo;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/LandingPage';
-import Logo from '../assets/images/logo.svg';
+import Logo from '../components/Logo';
 import Main from '../assets/images/main.svg';
 
 const LandingPage = () => {
 	return (
 		<Wrapper>
 			<nav>
-				<img src={Logo} alt="Jobify" className="logo" />
+				<Logo />
 			</nav>
 			<div className="container page">
 				<div className="info">
