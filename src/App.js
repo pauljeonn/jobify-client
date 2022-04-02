47c931ac08d6f865842f7ba83f42be971523da3a
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-04-02 22:41:19 +0900

    Feat: React Router 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
 function App() {
 	return (
-		<div className="App">
-			<LandingPage />
-		</div>
+		<BrowserRouter>
+			<nav>
+				<Link to="/">Dashboard</Link>
+				<Link to="/register">Register</Link>
+				<Link to="/landing">Landing</Link>
+			</nav>
+			<Routes>
+				<Route path="/landing" element={<LandingPage />} />
+				<Route path="*" element={<h1>This page does not exist!</h1>} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Wrapper from '../assets/wrappers/LandingPage';
 import Logo from '../components/Logo';
 import Main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
 	return (
@@ -13,7 +14,9 @@ const LandingPage = () => {
 				<div className="info">
 					<h1>Job Tracking App</h1>
 					<p>This is a great website!</p>
-					<button class="btn btn-hero">Login/Register</button>
+					<Link to="register" className="btn btn-hero">
+						Login/Register
+					</Link>
 				</div>
 				<img src={Main} alt="" className="main-img" />
 			</div>
