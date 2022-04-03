0a5fbaf839f912bc03ce2c2f9e26d0cd2d58a9cd
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-04-03 21:52:48 +0900

    Feat: ErrorPage 생성

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import NotFound from '../assets/images/not-found.svg';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	return (
+		<Wrapper>
+			<div>
+				<img src={NotFound} alt="" />
+				<h3>Oops! Page Not Found</h3>
+				<p>We can't find the page you're looking for.</p>
+				<Link to="/">Back Home</Link>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default ErrorPage;
