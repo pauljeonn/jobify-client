1c01d5b053f35431661e01314f53ec43b4a143f3
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-04-04 23:08:00 +0900

    Feat: RegisterPage 생성

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import Logo from '../components/Logo';
+
+const initialState = {
+	name: '',
+	email: '',
+	password: '',
+	isMember: true,
+};
 
 const RegisterPage = () => {
-	return <div>RegisterPage</div>;
+	const [values, setValues] = useState(initialState);
+	// global state and useNavigate
+	const handleChange = (e) => {
+		console.log(e.target);
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target);
+	};
+
+	return (
+		<Wrapper className="full-page">
+			<form className="form" onSubmit={onSubmit}>
+				<Logo />
+				<h3>Login</h3>
+				{/* Name input */}
+				<div className="form-row">
+					<label htmlFor="name" className="form-label">
+						Name
+					</label>
+					<input
+						type="text"
+						value={values.name}
+						name="name"
+						onChange={handleChange}
+						className="form-input"
+					/>
+					<button type="submit" className="btn btn-block">
+						Submit
+					</button>
+				</div>
+			</form>
+		</Wrapper>
+	);
 };
 
 export default RegisterPage;
